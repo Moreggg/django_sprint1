eea44df85642c083646cf7e31b4f8ce94552da12
Evgeny Cherednichenko <More1ggg@yandex.ru>
2024-02-12 19:26:16 +0300

    Add views and urls

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    template = 'blog/index.html'
+    return render(request, template)
+
+def post_detail(request):
+    pass
+
+def category_posts(request):
+    pass
